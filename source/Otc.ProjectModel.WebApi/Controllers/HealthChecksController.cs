f8380f79116a32371e7d5cf835cbf5916ac4555b
Luciano Lima <lima@lucianolima.com.br>
2018-07-30 16:56:43 -0300

    incluido controler de health

---
+++ source/Otc.ProjectModel.WebApi/Controllers/HealthChecksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Otc.ProjectModel.WebApi.Controllers
+{
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class HealthChecksController : ControllerBase
+    {
+        [HttpGet(ApplicationProperties.HealthzPath)]
+        public IActionResult Healthz()
+        {
+            return Ok(DateTimeOffset.Now);
+        }
+    }
+}
