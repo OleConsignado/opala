757f34d538317a2d74de8f7d23fe7340d8fc4294
Luciano Lima <lima@lucianolima.com.br>
2018-10-05 12:08:39 -0300

    incluido projeto web mvc padrão

--- Source/Opala.Web/Startup.cs
+++ Source/Opala.Web/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -43,13 +43,18 @@ namespace Opala.Web
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
